81837905741e973f8bd85e7be7c84a26f4b4981c
Chris Schafer <xophere@gmail.com>
2019-05-07 10:07:29 -0700

    sh changes and debug

--- certbotvault/updatecerts.sh
+++ certbotvault/updatecerts.sh
@@ -6,8 +6,7 @@
 
 # Grab route53 capable AWS key from vault
 
-check_errs()
-{
+check_errs() {
   # Function. Parameter 1 is the return code
   # Para. 2 is text to display on failure.
   if [ "${1}" -ne "0" ]; then
@@ -17,44 +16,47 @@ check_errs()
   fi
 }
 
-if [[ -z $AWS_ACCESS_KEY_ID ]]; then
+if [ "${AWS_ACCESS_KEY_ID+1}" ]; then
   check_errs $? "no AWS ACCESS KEY set"
 fi
 
-if [[ -z $AWS_SECRET_ACCESS_KEY]]; then 
+if [ "${AWS_SECRET_ACCESS_KEY+1}" ]; then 
   check_errs $? "no AWS SECRET KEY SET"
 fi
 
-if [[-z $ADMINEMAIL ]]; then
+if [ "${ADMINEMAIL+1}" ]; then
   check_errs $? "no ADMIN EMAIL SET"
 fi
-if [[ -z $DOMAIN ]]; then
+if [ "${DOMAIN+1}" ]; then
   check_errs $? "no domain name set"
+fi
 
-# Make cert request
+# Make cert request uses AWS key passed in from vault during nomad job start.
 certbot certonly \
-    -n --agree-tos --email $ADMINEMAIL \
+    -n --agree-tos --email "${ADMINEMAIL}" \
     --dns-route53 \
-    -d $DOMAIN
+    -d "${DOMAIN}"
 check_errs $? "certbot cert update errored"
 
 
+# Check vault envs are set....
+
 # Store result in vault
 
-if [[ -z $CERTPATH]]; then
-  vault kv put $CERTPATH key=@/etc/letsencrypt/live/orbtestenv.net/privkey.pem
+if [ "${CERTPATH+1}" ]; then
+  vault kv put ${CERTPATH} key=@/etc/letsencrypt/live/orbtestenv.net/privkey.pem
   check_errs $? "vault put errored for private key"
 else
   echo "CERTPATH not set"
-  exit (1) 
+  exit 1 
 fi
 
-if [[ -z $CHAINPATH]]; then
-  vault kv put $CHAINPATH key=@/etc/letsencrypt/live/orbtestenv.net/fullchain.pem
+if [ "${CHAINPATH+1}" ]; then
+  vault kv put ${CHAINPATH} key=@/etc/letsencrypt/live/orbtestenv.net/fullchain.pem
   check_errs $? "vault put errored for chain"
 else
   echo "CHAINPATH not set"
-  exit (1) 
+  exit 1 
 fi
 
 
